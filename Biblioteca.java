cf3980df61037bbbc77d9ff9f92e2b949688609e
Kobos <kcoboscaballero@gmail.com>
2025-12-17 16:38:05 -0300

    desarrollo de la biblioteca

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,3 +1,44 @@
+import java.util.ArrayList;
+
 public class Biblioteca {
-    
+    private ArrayList<Libro> libros;
+    private String nombre;
+
+    /**
+     * Creacion de una biblioteca vacia
+     */
+
+    public Biblioteca(String nombre) {
+        this.nombre = nombre;
+        this.libros = new ArrayList<>();
+    }
+
+    /**
+     * Agrega un libro a la biblioteca
+     */
+
+    public void agregarLibro(Libro libro) {
+        libros.add(libro);
+        System.out.println("Libro agregado: " + libro.getTitulo());
+    }
+
+    /**
+     * Muestra todos los libros
+     */
+
+    public void mostrarTodosLosLibros() {
+        System.out.println("\n=== Biblioteca: " + nombre + "===");
+        System.out.println("Total de libros: " + libros.size());
+    }
+
+    if (libros.isEmpty()) {
+        System.out.println("No hay libros en la biblioteca");
+        return;
+    }
+
+    for (int i = 0; i < libros.size(); i++) {
+        System.out.println("\n Libro #" + (i + 1));
+        Libro libro = libros.get(i);
+        libro.mostrarInfo();
+    }
 }
