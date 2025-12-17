09653e7bdba0d90b1a8ed3fd0c46bb898876f475
Kobos <kcoboscaballero@gmail.com>
2025-12-17 16:27:30 -0300

    desarrollo a la entra del programa

--- Main.java
+++ Main.java
@@ -1,3 +1,11 @@
 public class Main {
+    /**
+     * Entrada del programa
+     */
     
+    public static void main(String[] args) {
+        System.out.println("╔═══════════════════════════════╗");
+        System.out.println("║  SISTEMA DE BIBLIOTECA        ║");
+        System.out.println("╚═══════════════════════════════╝\n");
+    }
 }
